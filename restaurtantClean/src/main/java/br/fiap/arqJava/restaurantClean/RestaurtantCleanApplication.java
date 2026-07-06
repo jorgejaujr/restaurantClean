c22593f0927c8ae2ea2e288133f1321e0969ae4c
jorgejaujr <jorgejaujr@gmail.com>
2026-07-05 22:16:19 -0300

    First commit
    Commiting for the first time

---
+++ restaurtantClean/src/main/java/br/fiap/arqJava/restaurantClean/RestaurtantCleanApplication.java
@@ -0,0 +1,13 @@
+package br.fiap.arqJava.restaurantClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurtantCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurtantCleanApplication.class, args);
+	}
+
+}
---
+++ restaurtantClean/src/test/java/br/fiap/arqJava/restaurantClean/RestaurtantCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fiap.arqJava.restaurantClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurtantCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
